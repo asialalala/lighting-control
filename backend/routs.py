0831c7149cc1aa129b005396043f3083cdb36a9c
Joasia Klasik <asklas@op.pl>
2024-10-21 18:33:09 +0200

    Tests of API

--- backend/routs.py
+++ backend/routs.py
@@ -21,69 +21,68 @@ async def route_get_all_devices():
     return devices
 
 @app.route('/api/turn-on/<ip>')
-async def turn_on():
+async def route_turn_on(ip: string):
     print("Try to turn on")
-    dev = await Discover.discover_single("{ip}", username="asklas@op.pl", password="asia2002")
-    print('Turn on the light in device', "{ip}")
+    dev = await Discover.discover_single(ip, username="asklas@op.pl", password="asia2002")
+    print('Turn on the light in device', dev.modules)
     await dev.turn_on()
     await dev.update()
 
 @app.route('/api/turn-off/<ip>')
-async def turn_off():
+async def route_turn_off(ip: string):
     print("Try to turn off")
-    dev = await Discover.discover_single("{ip}", username="asklas@op.pl", password="asia2002")
+    dev = await Discover.discover_single(ip, username="asklas@op.pl", password="asia2002")
     print('Turn off the light in device', "{ip}")
     await dev.turn_off()
     await dev.update()
 
 @app.route('/api/set-temperature/<ip>', methods=['POST'])
-async def set_remperature():
+async def route_set_temperature():
     print("Try to set temperature")
     data = request.form
     temp = data['temperature']
     dev = await Discover.discover_single("{ip}", username="asklas@op.pl", password="asia2002")
-    print('Set temperature ', dev)
-    if Module.Light in dev.modules:
+    await dev.update()
+    print('Set temperature in device ', "{ip}")
+    if light := dev.modules.get("Light"):
         if light.is_color:
             print("Setting temperature...")
-            light = dev.modules[Module.Light]
             await light.set_color_temp(temp)
             await dev.update()
     else:
         print("Unable to set tremperature in this device.")
 
 @app.route('/api/set-colour/<ip>', methods=['POST'])
-async def set_colour(dev: Device):
+async def route_set_colour():
     print("Try to set colour")
     dev = await Discover.discover_single("{ip}", username="asklas@op.pl", password="asia2002")
-    print('Set colour ', dev)
+    await dev.update()
+    print('Set colour in device', "{ip}")
     data = request.form
     hue : int = data['hue']
     saturation : int = data['saturation']
     value : int = data['value']
     print('Set hue', hue, ' sat ', saturation, ' value ', value)
 
-    if Module.Light in dev.modules:
+    if light := dev.modules.get("Light"):
         if light.is_color:
             print("Setting colour...")
-            light = dev.modules[Module.Light]
             await light.set_hsv(hue=hue, saturation=saturation, value=value)
             await dev.update()
     else:
         print("Unable to set colour in this device.")
 
 @app.route('/api/set-brightness/<ip>', methods=['POST'])
-async def set_brightness(dev: Device):
+async def route_set_brightness():
     print("Try to set brightness")
     dev = await Discover.discover_single("{ip}", username="asklas@op.pl", password="asia2002")
-    print('Set brightness ', dev)
+    await dev.update()
+    print('Set brightness on device', "{ip}")
     data = request.form
     brightness : int = data['brightness']
     print('brightness ', brightness)
-
-    if Module.Light in dev.modules:
+    if light := dev.modules.get("Light"):
         print("Setting colour...")
-        light = dev.modules[Module.Light]
         await light.set_brightness(brightness)
         await dev.update()
     else:
