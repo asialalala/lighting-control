52fe7db2c10b3a83913cd80b8ed224ddb9af2ca8
Joasia Klasik <asklas@op.pl>
2024-10-22 23:03:10 +0200

    Correct name

--- frontend/src/app/services/bulb.service.ts
+++ frontend/src/app/services/bulb.service.ts
@@ -6,8 +6,6 @@ import { ApiService } from './api.service';
   providedIn: 'root',
 })
 export class BulbService {
-
-  apiService = inject(ApiService)
   
   bulbList: Bulb[] = [{
     id: 1,
@@ -114,40 +112,6 @@ export class BulbService {
       })
   }
 
-  async submitColor(hue: string, saturation: string, value: string) {
-    console.log("Submit color");
-    (await this.apiService.setColour("192.168.101.9",Number(hue), Number(saturation), Number(value) )).subscribe(
-      res => {
-        console.log("Res: ", res );
-      },
-      error => {
-        console.error('Error setting color: ', error)
-      }
-    )
-  }
-
-  async submitBrightness(brightness: string) {
-    console.log("submit")
-    console.log("Submit brightness");
-    (await this.apiService.setBrightness("192.168.101.9", Number(brightness))).subscribe(
-      res => {
-        console.log("Res:", res);
-      },
-      error => {
-        console.error('Error setting brightness: ', error);
-      })
-  }
-
-  async submitTemperature(temperature: string) {
-    console.log("Submit temperature");
-    (await this.apiService.setTemperature("192.168.101.9", Number(temperature))).subscribe(
-      res => {
-        console.log("Res: ", res);
-      },
-      error => {
-        console.error('Error setting temperatur: ', error)
-      })
-  }
   async submitColor(hue: string, saturation: string, value: string) {
     console.log("Submit color");
     (await this.apiService.setColour("192.168.101.9",Number(hue), Number(saturation), Number(value) )).subscribe(
