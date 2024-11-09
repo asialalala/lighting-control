d3d379138ce60aa7ce739f9efbc0351e020c3236
Joasia Klasik <asklas@op.pl>
2024-11-09 09:22:39 +0100

    Add 4 charts

--- frontend/src/app/components/chart/my-chart.component.ts
+++ frontend/src/app/components/chart/my-chart.component.ts
@@ -18,9 +18,18 @@ import {
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
-  chart: ApexChart;
-  xaxis: ApexXAxis;
+  chart: any; //ApexChart;
+  dataLabels: ApexDataLabels;
+  markers: ApexMarkers;
   title: ApexTitleSubtitle;
+  fill: ApexFill;
+  yaxis: ApexYAxis;
+  xaxis: ApexXAxis;
+  tooltip: ApexTooltip;
+  stroke: ApexStroke;
+  grid: any; //ApexGrid;
+  colors: any;
+  toolbar: any;
 };
 
 @Component({
@@ -32,26 +41,198 @@ export type ChartOptions = {
 })
 export class MyChartComponent {
   @ViewChild("chart") chart: ChartComponent | undefined;
-  public chartOptions: Partial<ChartOptions>;
+  public chart1options: Partial<ChartOptions>;
+  public chart2options: Partial<ChartOptions>;
+  public chart3options: Partial<ChartOptions>;
+  public chart4options: Partial<ChartOptions>;
+  public commonOptions: Partial<ChartOptions> = {
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: "straight"
+    },
+    toolbar: {
+      tools: {
+        selection: false
+      }
+    },
+    markers: {
+      size: 6,
+      hover: {
+        size: 10
+      }
+    },
+    tooltip: {
+      followCursor: false,
+      theme: "dark",
+      x: {
+        show: false
+      },
+      marker: {
+        show: false
+      },
+      y: {
+        title: {
+          formatter: function() {
+            return "aaaa";
+          }
+        }
+      }
+    },
+    grid: {
+      clipMarkers: false
+    },
+    xaxis: {
+      type: "datetime"
+    }
+  };
   
   constructor() {
-    this.chartOptions = {
+    this.chart1options = {
+      title : {
+        text : "Napięcie"
+      },
       series: [
         {
-          name: "My-series",
-          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+          name: "chart1",
+          data: this.generateDayWiseTimeSeries(
+            new Date("11 Feb 2017").getTime(),
+            20,
+            {
+              min: 10,
+              max: 60
+            }
+          )
         }
       ],
       chart: {
-        height: 350,
-        type: "bar"
+        id: "fb",
+        group: "social",
+        type: "line",
+        height: 300
       },
-      title: {
-        text: "My First Angular Chart"
+      colors: ["#008FFB"],
+      yaxis: {
+        tickAmount: 2,
+        labels: {
+          minWidth: 40
+        }
+      }
+    };
+
+    this.chart2options = {
+      title : {
+        text : "Zużycie prądu"
       },
-      xaxis: {
-        categories: ["Jan", "Feb",  "Mar",  "Apr",  "May",  "Jun",  "Jul",  "Aug", "Sep"]
+      series: [
+        {
+          name: "chart2",
+          data: this.generateDayWiseTimeSeries(
+            new Date("11 Feb 2017").getTime(),
+            20,
+            {
+              min: 10,
+              max: 30
+            }
+          )
+        }
+      ],
+      chart: {
+        id: "tw",
+        group: "social",
+        type: "line",
+        height: 300
+      },
+      colors: ["#546E7A"],
+      yaxis: {
+        tickAmount: 2,
+        labels: {
+          minWidth: 40
+        }
+      }
+    };
+
+    this.chart3options = {
+      title : {
+        text : "Zużycie mocy"
+      },
+      series: [
+        {
+          name: "chart3",
+          data: this.generateDayWiseTimeSeries(
+            new Date("11 Feb 2017").getTime(),
+            20,
+            {
+              min: 10,
+              max: 60
+            }
+          )
+        }
+      ],
+      chart: {
+        id: "yt",
+        group: "social",
+        type: "area",
+        height: 300
+      },
+      colors: ["#00E396"],
+      yaxis: {
+        tickAmount: 2,
+        labels: {
+          minWidth: 40
+        }
+      }
+    };
+
+    this.chart4options = {
+      title : {
+        text : "Jasność żarówki"
+      },
+      series: [
+        {
+          name: "chart4",
+          data: this.generateDayWiseTimeSeries(
+            new Date("11 Feb 2017").getTime(),
+            20,
+            {
+              min: 10,
+              max: 60
+            }
+          )
+        }
+      ],
+      chart: {
+        id: "yt",
+        group: "social",
+        type: "area",
+        height: 300
+      },
+      colors: ["#00E396"],
+      yaxis: {
+        tickAmount: 2,
+        labels: {
+          minWidth: 40
+        }
       }
     };
   }
+
+
+  public generateDayWiseTimeSeries(baseval : number, count : number, yrange : {min : number, max : number}): any[] {
+    let i = 0;
+    let series = [];
+    while (i < count) {
+      var x = baseval;
+      var y =
+        Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+
+      series.push([x, y]);
+      baseval += 86400000;
+      i++;
+    }
+    return series;
+  }
+
 }
+
